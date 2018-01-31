8530a1d93b357927f97013591ec829f70fe99ac2
PorfirioTramontana <porfirio.tramontana@gmail.com>
2018-01-31 13:09:50 +0100

    Added CoverageEvaluation package with utility functions supporting Coverage Colection and (in future) coverage evaluation.
    Modified MMAnageTrain.jave to support coverage evaluation

--- src/RandomMain.java
+++ src/RandomMain.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 
 import input.InputData;
 import control.DSTM;
+import coverageEvaluation.StateTransitionCoverage;
 import strategy.DeterministicStrategy;
 import strategy.Strategy;
 import testCaseGenerator.TestCaseGenerator;
@@ -13,15 +14,18 @@ public class RandomMain {
 	public static void main(String[] args) throws IOException {
 		//Strategy s = new RandomStrategy();
 		InputData testBase=new InputData(".\\testdata.csv");
-		int length=100;
-		int numTestCase=1000;
+		int length=1000;
+		int numTestCase=100;
 		String technique="UniformRandom";
 		String outputFolder=new String(".\\testsuite\\");
+		String outputCoverageFolder= new String(".\\coverage");
 		new File(outputFolder).mkdir();
+		new File(outputCoverageFolder).mkdir();
 		//String outputFolder=new String("");
 		TestCaseGenerator.generate(testBase,length,numTestCase,technique,outputFolder);
 		
 		for (int i=0;i<numTestCase;i++) {
+			StateTransitionCoverage.openFile(outputCoverageFolder,"testcoverage"+Integer.toString(i)+".csv");
 			Strategy s= new DeterministicStrategy(new InputData(outputFolder+"testdata"+Integer.toString(i)+".csv"));
 			
 			for (int j = 0; j < 1; j++) {
@@ -32,6 +36,7 @@ public class RandomMain {
 				}
 				System.out.println("---END---");
 			}
+			StateTransitionCoverage.closeFile();
 		}
 	}
 	
--- src/control/MManageTrain.java
+++ src/control/MManageTrain.java
@@ -2,6 +2,7 @@ package control;
 
 import java.util.ArrayList;
 
+import coverageEvaluation.StateTransitionCoverage;
 import general.ExternalChannel;
 import general.Machine;
 import general.Semaphore;
@@ -43,7 +44,9 @@ public class MManageTrain extends Machine {
 					switch (s) {
 					case initial:						
 						System.out.println("current node = " + name + "_initial");
+						StateTransitionCoverage.writeCoverage(name+",S,"+s.name());
 						if (true) {
+							StateTransitionCoverage.writeCoverage(name+",T,1");
 							System.out.println("firing transition = " + name + "_T01");
 							state = State.idle;
 							System.out.println("next node = " + name + "_idle");
@@ -53,8 +56,10 @@ public class MManageTrain extends Machine {
 						}
 						break;
 					case idle:
+						StateTransitionCoverage.writeCoverage(name+",S,"+s.name());
 						System.out.println("current node = " + name + "_idle");
 						if (true) {
+							StateTransitionCoverage.writeCoverage(name+",T,2");
 							System.out.println("firing transition = " + name + "_T02");
 							Machine m = new MSessionEstablishment(this, control.MSessionEstablishment.State.initial, PchFrom, PchTo);
 							this.registerAsChildren(State.establishement,m);
@@ -66,23 +71,27 @@ public class MManageTrain extends Machine {
 						}
 						break;
 					case establishement:
+						StateTransitionCoverage.writeCoverage(name+",S,"+s.name());
 						System.out.println("current node = " + name + "_establishement");
 						if (this.terminatedChildred.get(State.establishement).size()>0) {
 							Machine m = this.terminatedChildred.get(State.establishement).remove(0);
 							MSessionEstablishment.State exitingState = (control.MSessionEstablishment.State) this.exitingState.remove(m);
 							if (exitingState == control.MSessionEstablishment.State.entry) {
+								StateTransitionCoverage.writeCoverage(name+",T,3");
 								System.out.println("firing transition = " + name + "_T03");
 								state = State.exiting;
 								System.out.println("next node = " + name + "_exiting");
 								System.out.println("**********");
 								this.notifyTerminationToParent();
 							} else if (exitingState == control.MSessionEstablishment.State.som) {
+								StateTransitionCoverage.writeCoverage(name+",T,4");
 								System.out.println("firing transition = " + name + "_T04");
 								state = State.exiting;
 								System.out.println("next node = " + name + "_exiting");
 								System.out.println("**********");
 								this.notifyTerminationToParent();
 							} else if (exitingState == control.MSessionEstablishment.State.aborted) {
+								StateTransitionCoverage.writeCoverage(name+",T,7");
 								System.out.println("firing transition = " + name + "_T07");
 								state = State.exiting;
 								System.out.println("next node = " + name + "_exiting");
@@ -95,18 +104,21 @@ public class MManageTrain extends Machine {
 						}
 						break;
 					case entry:
+						StateTransitionCoverage.writeCoverage(name+",S,"+s.name());
 						System.out.println("current node = " + name + "_entry");
 						System.out.println("**********");
 						/*assignTokenToChildren();*/
 						break;
 						
 					case som:
+						StateTransitionCoverage.writeCoverage(name+",S,"+s.name());
 						System.out.println("current node = " + name + "_som");
 						System.out.println("**********");
 						/*assignTokenToChildren();*/
 						break;
 					
 					case exiting:
+						StateTransitionCoverage.writeCoverage(name+",S,"+s.name());
 						System.out.println("current node = " + name + "_exiting");
 						System.out.println("**********");
 						assignTokenToChildren();
